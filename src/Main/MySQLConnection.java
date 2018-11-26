ff54641e28e9ddda74ac597d9ddff84b4cc7a17f
Wiguna R <zombozo12@users.noreply.github.com>
2018-11-27 00:46:22 +0700

    First Commit

---
+++ src/Main/MySQLConnection.java
@@ -0,0 +1,27 @@
+package Main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Wiguna R
+ */
+public class MySQLConnection {
+    Connection conn = null;
+    
+    public static Connection ConnectDb(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db_sysbook","root","jancok");
+            return conn;
+        }catch(ClassNotFoundException | SQLException ex){
+            JOptionPane.showMessageDialog(null, ex);
+            JOptionPane.showMessageDialog(null, "Not Connected to Databases/Turn on Your Xampp");
+            return null;
+        }
+    
+    }
+}
